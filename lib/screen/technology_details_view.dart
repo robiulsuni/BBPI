d4a05bee94fcfeed2c4338e1f6be8a4251289c33
Md. Robiul Suni <85034871+robiulsuni@users.noreply.github.com>
2022-01-26 15:05:19 +0600

    teacher json

--- lib/screen/technology_details_view.dart
+++ lib/screen/technology_details_view.dart
@@ -10,7 +10,7 @@ class TechnologyDetailsView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Computer Technology'),
+        title: Text('Technology Details'),
       ),
       body: SingleChildScrollView(
         child: Padding(
