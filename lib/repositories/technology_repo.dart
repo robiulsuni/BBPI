e79afdd093f18a9a8df1565ba81608c49106a54c
Md. Robiul Suni <85034871+robiulsuni@users.noreply.github.com>
2022-01-26 11:43:55 +0600

    comment out print

--- lib/repositories/technology_repo.dart
+++ lib/repositories/technology_repo.dart
@@ -5,7 +5,7 @@ import '../models/technology_model.dart';
 class TechnologRepo {
   static Future<List<Technologmodel>> getTechnology() async {
     final response = await rootBundle.loadString('assets/json/technology.json');
-    print(response);
+    // print(response);
     if (response == null) {
       return [];
     } else {
