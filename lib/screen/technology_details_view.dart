6222fd57640388739f8ed9ac6280e5d16369e090
Md. Robiul Suni <85034871+robiulsuni@users.noreply.github.com>
2022-01-26 11:43:31 +0600

    technology view entry code

---
+++ lib/screen/technology_details_view.dart
@@ -0,0 +1,30 @@
+import 'package:bbpi/models/technology_model.dart';
+import 'package:flutter/material.dart';
+
+class TechnologyDetailsView extends StatelessWidget {
+  const TechnologyDetailsView({Key? key, required this.technologmodel})
+      : super(key: key);
+  final Technologmodel technologmodel;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Computer Technology'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Image.asset('${technologmodel.img}'),
+              Center(child: Text('${technologmodel.computer}')),
+              Center(child: Text('${technologmodel.departmentinfo}')),
+              Center(child: Text('${technologmodel.lab}')),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
